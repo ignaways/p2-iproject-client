1baa3a0f871148b81aca3f11dc5c86bb8c03f2e7
ignatius andri pratama putra <ignaway@gmail.com>
2022-03-24 12:32:03 +0700

    baseUrl

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL:'http://localhost:3000'
+    baseURL:'https://iproject-ignaways.herokuapp.com/'
 })
 
 export default instance
